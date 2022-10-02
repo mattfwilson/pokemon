1f146f71a5dcb5c034765deca4d09f34c31112f4
mattfwilson <mattfwilson@gmail.com>
2022-10-02 18:39:40 -0400

    Simple level detection added.

--- fight.py
+++ fight.py
@@ -1,27 +1,30 @@
 import random
 
-exp = 0
-
-levels = {
+EXP = 0
+LEVELS = {
     1:10,
     2:20,
     3:35,
     4:50,
-    5:75
+    5:75,
+    6:100,
+    7:140
     }
 
-def roll_exp():
-    global exp
+def roll_exp(levels):
+    global EXP
     gained = random.randint(4, 8)
-    exp = gained + exp
-    return exp, gained
+    EXP = gained + EXP
+    for level in levels:
+        if levels[level] <= EXP:
+            print(f'You reached level {level}')
+    return EXP, gained
 
 while True:
-
     fight = str(input('Fight? '))
 
     if fight == 'yes':
-        res = roll_exp()
+        res = roll_exp(LEVELS)
         print(f'EXP Gained: {res[1]}')
         print(f'Total EXP: {res[0]}')
     else:
---
+++ fight2.py
@@ -0,0 +1,27 @@
+import random
+
+exp = 0
+levels = {
+    1:10,
+    2:20,
+    3:35,
+    4:50,
+    5:75
+    }
+
+def roll_exp():
+    global exp
+    gained = random.randint(4, 8)
+    exp = gained + exp
+    return exp, gained
+
+while True:
+
+    fight = str(input('Fight? '))
+
+    if fight == 'yes':
+        res = roll_exp()
+        print(f'EXP Gained: {res[1]}')
+        print(f'Total EXP: {res[0]}')
+    else:
+        quit()
