9d10f50f2435066c54c3826e127bd56a4789d3b8
mattfwilson <mattfwilson@gmail.com>
2022-10-02 22:22:26 -0400

    Fixed leveling so it only displays once per loop. Uses list index as level indicator.

--- fight.py
+++ fight.py
@@ -1,6 +1,6 @@
 import random
 
-EXP = 0
+EXP = 15
 LEVELS = {
     1:10,
     2:20,
@@ -11,21 +11,23 @@ LEVELS = {
     7:140
     }
 
+
 def roll_exp(levels):
     global EXP
     gained = random.randint(4, 8)
     EXP = gained + EXP
     for level in levels:
         if levels[level] <= EXP:
-            print(f'You reached level {level}')
-    return EXP, gained
+            print(level)
+    return gained, EXP
 
 while True:
     fight = str(input('Fight? '))
 
     if fight == 'yes':
         res = roll_exp(LEVELS)
-        print(f'EXP Gained: {res[1]}')
-        print(f'Total EXP: {res[0]}')
+        print(res)
+        print(f'EXP Gained: {res[0]}')
+        print(f'Total EXP: {res[1]}')
     else:
         quit()
--- fight2.py
+++ fight2.py
@@ -1,27 +1,25 @@
 import random
 
-exp = 0
-levels = {
-    1:10,
-    2:20,
-    3:35,
-    4:50,
-    5:75
-    }
+EXP = 0
+LEVELS = [0, 10, 20, 35, 50, 75, 100, 140, 200]
 
 def roll_exp():
-    global exp
+    global EXP
+    global LEVELS
     gained = random.randint(4, 8)
-    exp = gained + exp
-    return exp, gained
+    EXP = gained + EXP
+    for level in LEVELS:
+        if EXP <= level:
+            return LEVELS.index(level), gained, EXP
 
 while True:
-
     fight = str(input('Fight? '))
 
     if fight == 'yes':
         res = roll_exp()
+        print(res)
+        print(f'Current level: {res[0]}')
         print(f'EXP Gained: {res[1]}')
-        print(f'Total EXP: {res[0]}')
+        print(f'Total EXP: {res[2]}')
     else:
         quit()
