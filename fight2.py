4298197cd8bbcfd087a8e66e552d968d65567ef6
mattfwilson <mattfwilson@gmail.com>
2023-07-31 22:40:08 -0400

    Second version

--- fight2.py
+++ fight2.py
@@ -2,6 +2,7 @@ import random
 
 EXP = 0
 LEVELS = [0, 10, 20, 35, 50, 75, 100, 140, 200]
+ACTIONS = ['Rock', 'Scissors', 'Paper']
 
 def roll_exp():
     global EXP
@@ -12,14 +13,41 @@ def roll_exp():
         if EXP <= level:
             return LEVELS.index(level), gained, EXP
 
-while True:
-    fight = str(input('Fight? '))
+def check_outcome(player_choice):
+    global ACTIONS
+    
+    p_choice = ACTIONS.index(player_choice)
+    print(f'{p_choice} - Player chose {player_choice}.')
 
-    if fight == 'yes':
-        res = roll_exp()
-        print(res)
-        print(f'Current level: {res[0]}')
-        print(f'EXP Gained: {res[1]}')
-        print(f'Total EXP: {res[2]}')
+    comp_choice = random.choice(ACTIONS)
+    c_choice = ACTIONS.index(comp_choice)
+    print(f'{c_choice} - Computer chose {comp_choice}.')
+
+    if c_choice + 1 % 3 == p_choice:
+        print(f'You lose!')
+    elif p_choice + 1 % 3 == c_choice:
+        print(f'You win!')
     else:
-        quit()
+        print(f'Tie.')
+
+while True:
+    action = str(input('Fight? '))
+    check_outcome(action)
+
+    # if action == 'rock':
+    #     pass
+    # elif action == 'paper':
+    #     pass
+    # elif action == 'scissors':
+    #     pass
+    # else:
+    #     quit()
+
+    # if fight == 'yes':
+    #     result = roll_exp()
+    #     print(result)
+    #     print(f'Current level: {result[0]}')
+    #     print(f'EXP Gained: {result[1]}')
+    #     print(f'Total EXP: {result[2]}')
+    # else:
+    #     quit()
